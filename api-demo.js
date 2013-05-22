c3e9d93570388c11cf42a12ce21d27442f18d926
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-05-22 19:33:18 +0200

    Syntax highlighting and interactive demos

---
+++ api-demo.js
@@ -0,0 +1,48 @@
+// TODO convert to a WP plugin?
+
+(function() {
+	$( ".entry-example" ).each(function() {
+		var iframeSrc,
+			src = $( this ).find( ".syntaxhighlighter" ),
+			output = $( this ).find( ".code-demo" );
+
+		if ( !src.length || !output.length ) {
+			return;
+		}
+
+		// Get the original source
+		iframeSrc = src.find( "td.code .line" ).map(function() {
+			// Convert non-breaking spaces from highlighted code to normal spaces
+			return $( this ).text().replace( /\xa0/g, " " );
+		// Restore new lines from original source
+		}).get().join( "\n" );
+
+		iframeSrc = iframeSrc
+			// Insert styling for the live demo that we don't want in the
+			// highlighted code
+			.replace( "</head>",
+				"<style>" +
+					"html, body { border:0; margin:0; padding:0; }" +
+					"body { font-family: 'Helvetica', 'Arial',  'Verdana', 'sans-serif'; }" +
+				"</style>" +
+				"</head>" )
+			// IE <10 executes scripts in the order in which they're loaded,
+			// not the order in which they're written. So we need to defer inline
+			// scripts so that scripts which need to be fetched are executed first.
+			.replace( /<script>([\s\S]+)<\/script>/,
+				"<script>" +
+				"window.onload = function() {" +
+					"$1" +
+				"};" +
+				"</script>" );
+
+		var iframe = document.createElement( "iframe" );
+		iframe.width = "100%";
+		iframe.height = output.attr( "data-height" ) || 250;
+		output.append( iframe );
+
+		var doc = (iframe.contentWindow || iframe.contentDocument).document;
+		doc.write( iframeSrc );
+		doc.close();
+	});
+}());
---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+function scripts() {
+    echo '<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>' . "\n";
+    echo '<script src="' . get_stylesheet_directory_uri() . '/api-demo.js"></script>' . "\n";
+}
+add_action( 'wp_footer', 'scripts' );
+?>
